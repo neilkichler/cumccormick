01ceb9113573a38eb41cf7fb88f23126c1e33948
Neil Kichler <neil.kichler@gmail.com>
2024-05-08 18:18:26 +0200

    add randomized tests

--- tests/tests_randomized.cu
+++ tests/tests_randomized.cu
@@ -23,7 +23,8 @@ constexpr int THREADS_PER_BLOCK = 64;
 constexpr int TOTAL_THREADS     = BLOCK_COUNT * THREADS_PER_BLOCK;
 constexpr int VECTOR_SIZE       = 64;
 
-constexpr int n_dims = 4;
+// we need 5 values: x.box.lb, x.cv, x.cc, x.box.ub, and x_interior
+constexpr int n_dims = 5;
 
 __global__ void setup_randomized_kernel(u64 *sobol_directions,
                                         u64 *sobol_scrambled_constants,
@@ -40,22 +41,104 @@ __global__ void setup_randomized_kernel(u64 *sobol_directions,
     }
 }
 
-__global__ void generate_kernel(rng_state *state, int n)
+template<typename T>
+__device__ void check_univariate(mc<T> x, T *interior_samples, int n)
 {
-    int idx = threadIdx.x + blockIdx.x * THREADS_PER_BLOCK;
-    int dim = n_dims * idx;
-
-    mc<double> x;
+    auto contains = [x](mc<T> y_mc, T y, T x_sample) {
+        if (!(y_mc.cv <= y && y <= y_mc.cc)) {
+            printf("[E] Invalid bounds: y.cv = %.15g, y = %.15g, y.cc = %.15g\n", y_mc.cv, y, y_mc.cc);
+            printf("[E] Invalid bounds: y.cv = %a, y = %a, y.cc = %a\n", y_mc.cv, y, y_mc.cc);
+            printf("[E] For input: x_sample = %.15g, x = [%.15g, (%.15g, %.15g), %.15g]\n", x_sample, x.box.lb, x.cv, x.cc, x.box.ub);
+            printf("[E] For input: x_sample = %a, x = [%a, (%a, %a), %a]\n", x_sample, x.box.lb, x.cv, x.cc, x.box.ub);
+        }
+        return y_mc.cv <= y && y <= y_mc.cc;
+    };
 
     for (int i = 0; i < n; i++) {
-        x.cv     = curand_uniform_double(&state[dim + 0]);
-        x.cc     = curand_uniform_double(&state[dim + 1]);
-        x.box.lb = curand_uniform_double(&state[dim + 2]);
-        x.box.ub = curand_uniform_double(&state[dim + 3]);
-
-        if (x.cv <= x.cc && x.box.lb <= x.box.ub) {
-            printf("[%d][%d] [%g, (%g, %g), %g]\n", dim, i, x.box.lb, x.cv, x.cc, x.box.ub);
+        T x_sample = interior_samples[i];
+        assert(contains(pow(x, 1), x_sample, x_sample));
+        // assert(contains(pow(x, 2), pow(x_sample, 2), x_sample));
+        assert(contains(pow(x, 3), pow(x_sample, 3), x_sample));
+        assert(contains(pow(x, 4), pow(x_sample, 4), x_sample));
+        assert(contains(pow(x, 5), pow(x_sample, 5), x_sample));
+        assert(contains(abs(x), abs(x_sample), x_sample));
+        assert(contains(exp(x), exp(x_sample), x_sample));
+        assert(contains(fabs(x), fabs(x_sample), x_sample));
+        assert(contains(neg(x), -x_sample, x_sample));
+        // assert(contains(sqr(x), pow(x_sample, 2), x_sample));
+        // assert(contains(cos(x), cos(x_sample), x_sample));
+        // assert(contains(sin(x), sin(x_sample), x_sample));
+        if (inf(x) >= 0) {
+            assert(contains(log(x), log(x_sample), x_sample));
+            // assert(contains(recip(x), pow(x_sample, -1), x_sample));
+            assert(contains(sqrt(x), sqrt(x_sample), x_sample));
         }
+    }
+}
+
+template<typename T>
+__device__ void check_bivariate(mc<T> x, mc<T> y, T *interior_x_samples, T *interior_y_samples, int n)
+{
+    auto contains = [](mc<T> x, T y) {
+        return x.cv <= y && y <= x.cc;
+    };
+
+    for (int i = 0; i < n; i++) {
+        T x_sample = interior_x_samples[i];
+        T y_sample = interior_y_samples[i];
+
+        assert(contains(x + y, x_sample + y_sample));
+        assert(contains(x - y, x_sample - y_sample));
+        assert(contains(x * y, x_sample * y_sample));
+        assert(contains(max(x, y), max(x_sample, y_sample)));
+        assert(contains(min(x, y), min(x_sample, y_sample)));
+    }
+}
+
+__global__ void generate_and_check(rng_state *state, int n)
+{
+    // TODO: relax sampling to allow intervals that are tighter than the McCormick bound.
+    auto random_mccormick = [](rng_state *state, int offset) {
+        mc<double> x;
+        x.cv = curand_uniform_double(&state[offset + 0]);
+        do {
+            x.cc = curand_uniform_double(&state[offset + 1]);
+        } while (x.cv > x.cc);
+
+        do {
+            x.box.lb = curand_uniform_double(&state[offset + 2]);
+        } while (x.box.lb > x.cv);
+
+        do {
+            x.box.ub = curand_uniform_double(&state[offset + 3]);
+        } while (x.box.ub < x.cc);
+        return x;
+    };
+
+    auto random_interior_samples = [](rng_state *state, mc<double> x, double *samples, int n, int key) {
+        for (int j = 0; j < n; j++) {
+            samples[j] = x.cv + (x.cc - x.cv) * curand_uniform_double(&state[key]);
+        }
+    };
+
+    int x_idx = threadIdx.x + blockIdx.x * THREADS_PER_BLOCK;
+    int x_dim = n_dims * x_idx;
+
+    int y_idx = threadIdx.y + blockIdx.y * THREADS_PER_BLOCK;
+    int y_dim = n_dims * y_idx;
+
+    for (int i = 0; i < n; i++) {
+        mc<double> x = random_mccormick(state, x_dim);
+
+        constexpr int n_interior_samples = 128;
+        double interior_x_samples[n_interior_samples];
+        random_interior_samples(state, x, interior_x_samples, n_interior_samples, x_dim + 4);
+        check_univariate(x, interior_x_samples, n_interior_samples);
+
+        mc<double> y = random_mccormick(state, threadIdx.y + blockIdx.y * THREADS_PER_BLOCK);
+        double interior_y_samples[n_interior_samples];
+        random_interior_samples(state, y, interior_y_samples, n_interior_samples, y_dim + 4);
+        check_bivariate(x, y, interior_x_samples, interior_y_samples, n_interior_samples);
     }
 }
 
@@ -74,7 +157,6 @@ void tests_randomized(cudaStream_t stream, cudaEvent_t event)
     CURAND_CHECK(curandGetDirectionVectors64(&h_directions, CURAND_SCRAMBLED_DIRECTION_VECTORS_64_JOEKUO6));
     CURAND_CHECK(curandGetScrambleConstants64(&h_scrambled_constants));
 
-    // we need 4 values x.lb x.cv x.cc x.ub with restriction x.lb <=  x.ub, x.cv <= x.cc
     CUDA_CHECK(cudaMalloc(&d_directions, n_dims * TOTAL_THREADS * VECTOR_SIZE * sizeof(*d_directions)));
     CUDA_CHECK(cudaMemcpy(d_directions, h_directions, n_dims * TOTAL_THREADS * VECTOR_SIZE * sizeof(*d_directions), cudaMemcpyHostToDevice));
 
@@ -84,10 +166,11 @@ void tests_randomized(cudaStream_t stream, cudaEvent_t event)
     setup_randomized_kernel<<<BLOCK_COUNT, THREADS_PER_BLOCK, 0, stream>>>(d_directions, d_scrambled_constants, states);
 
     constexpr int n_iterations = 1;
-    constexpr int n_samples    = 1024;
+    constexpr int n_samples    = 512;
 
     for (int i = 0; i < n_iterations; i++) {
-        generate_kernel<<<BLOCK_COUNT, THREADS_PER_BLOCK, 0, stream>>>(states, n_samples);
+        // TODO: We can skip ahead in the random number generator to run these kernels in parallel streams
+        generate_and_check<<<BLOCK_COUNT, THREADS_PER_BLOCK, 0, stream>>>(states, n_samples);
     }
 
     CUDA_CHECK(cudaFree(states));
