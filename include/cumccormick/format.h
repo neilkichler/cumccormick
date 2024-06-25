9d4731787a1327a96f8868f0552efcc93803d1e9
Neil Kichler <neil.kichler@gmail.com>
2024-06-25 19:06:12 +0200

    add basic formatting

---
+++ include/cumccormick/format.h
@@ -0,0 +1,19 @@
+#ifndef CUMCCORMICK_FORMAT_H
+#define CUMCCORMICK_FORMAT_H
+
+#include <cumccormick/cumccormick.h>
+
+#include <ostream>
+
+namespace cu
+{
+
+template<typename T>
+std::ostream &operator<<(std::ostream &os, cu::mccormick<T> x)
+{
+    return os << "[ lb: " << inf(x) << ", (" << x.cv << ", " << x.cc << "), ub: " << sup(x) << "]";
+}
+
+} // namespace cu
+
+#endif
