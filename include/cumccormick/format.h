75282c7c9d39848de674ea85be9de5230a40a259
Neil Kichler <neil.kichler@gmail.com>
2024-07-02 16:52:47 +0200

    improve McCormick string formatting

--- include/cumccormick/format.h
+++ include/cumccormick/format.h
@@ -11,7 +11,7 @@ namespace cu
 template<typename T>
 std::ostream &operator<<(std::ostream &os, cu::mccormick<T> x)
 {
-    return os << "[ lb: " << inf(x) << ", (" << x.cv << ", " << x.cc << "), ub: " << sup(x) << "]";
+    return os << "[lb: " << inf(x) << ", (cv: " << x.cv << ", cc: " << x.cc << "), ub: " << sup(x) << "]";
 }
 
 } // namespace cu
