01b9f50f839f76df322b50f823686d8962b87193
Neil Kichler <neil.kichler@gmail.com>
2024-08-29 10:36:40 +0200

    [no ci] update profile.sh

--- tools/profile.sh
+++ tools/profile.sh
@@ -2,5 +2,4 @@
 # --backtrace=lbr 
 # --sample=process-tree 
 # --trace=cuda,nvtx,osrt
-# --cuda-graph-trace=graph
-nsys profile --cuda-memory-usage=true --vulkan-gpu-workload=false --opengl-gpu-workload=false --trace='cuda,osrt' $1
+nsys profile --cuda-graph-trace=node --cuda-memory-usage=true --vulkan-gpu-workload=false --opengl-gpu-workload=false --trace='cuda,osrt' $1
