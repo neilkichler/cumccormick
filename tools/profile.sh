d676a50649efdfb1fad88986a1d445c1e3df78da
Neil Kichler <neil.kichler@gmail.com>
2024-09-02 18:10:51 +0200

    add alternative option

--- tools/profile.sh
+++ tools/profile.sh
@@ -3,3 +3,4 @@
 # --sample=process-tree 
 # --trace=cuda,nvtx,osrt
 nsys profile --cuda-graph-trace=node --cuda-memory-usage=true --vulkan-gpu-workload=false --opengl-gpu-workload=false --trace='cuda,osrt' $1
+# nsys profile --cuda-graph-trace=graph --cuda-memory-usage=true --vulkan-gpu-workload=false --opengl-gpu-workload=false --trace='cuda,osrt' $1
