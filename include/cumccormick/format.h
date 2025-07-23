9fc6625a4e0842def4abb5020008313702c6fb6e
Neil Kichler <neil.kichler@gmail.com>
2025-07-23 15:38:10 +0200

    add std::format support

--- include/cumccormick/format.h
+++ include/cumccormick/format.h
@@ -3,6 +3,7 @@
 
 #include <cumccormick/cumccormick.h>
 
+#include <format>
 #include <ostream>
 
 namespace cu
@@ -15,5 +16,24 @@ std::ostream &operator<<(std::ostream &os, cu::mccormick<T> x)
 }
 
 } // namespace cu
+
+template<typename T>
+struct std::formatter<cu::mccormick<T>> : std::formatter<T>
+{
+    auto format(const cu::mccormick<T> &x, std::format_context &ctx) const
+    {
+        auto out = ctx.out();
+
+        out = std::format_to(out, "[");
+        out = std::formatter<T>::format(x.box.lb, ctx);
+        out = std::format_to(out, ", (");
+        out = std::formatter<T>::format(x.cv, ctx);
+        out = std::format_to(out, ", ");
+        out = std::formatter<T>::format(x.cc, ctx);
+        out = std::format_to(out, "), ");
+        out = std::formatter<T>::format(x.box.ub, ctx);
+        return std::format_to(out, "]");
+    }
+};
 
 #endif
